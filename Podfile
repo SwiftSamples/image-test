63833722ab49476b35b6a7a538d06a0e3ed504f3
Swift <samplefortest1234@gmail.com>
2020-11-21 13:22:59 +0530

    initial commit

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+
+
+target 'image' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for image
+
+
+pod "GSImageViewerController"
+
+
+
+
+  target 'imageTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'imageUITests' do
+    # Pods for testing
+  end
+
+end
--- image/ViewController.swift
+++ image/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
-
+import GSImageViewerController
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var tapImage: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        tapImage.isUserInteractionEnabled = true
+        tapImage.addGestureRecognizer(tapGestureRecognizer)
     }
+
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
+    {
+      //  let tappedImage = tapGestureRecognizer.view as! UIImageView
+        let imageInfo = GSImageInfo(image: tapImage.image!, imageMode: .aspectFit)
+        let transitionInfo = GSTransitionInfo(fromView: tapImage)
+        
+        let imageViewer    = GSImageViewerController(imageInfo: imageInfo, transitionInfo: transitionInfo)
+    
+        present(imageViewer, animated: true, completion: nil)
+    }
+        
+//        let tappedImage = tapGestureRecognizer.view as! UIImageView
+//        let newImageView = UIImageView(image: tappedImage.image)
+//        newImageView.frame = UIScreen.main.bounds
+//        newImageView.backgroundColor = .black
+//        newImageView.contentMode = .scaleAspectFit
+//        newImageView.isUserInteractionEnabled = true
+//        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
+//        newImageView.addGestureRecognizer(tap)
+//        self.view.addSubview(newImageView)
+//        self.navigationController?.isNavigationBarHidden = true
+//        self.tabBarController?.tabBar.isHidden = true
+//    }
+//
+//    @objc func dismissFullscreenImage(_ sender: UITapGestureRecognizer) {
+//        self.navigationController?.isNavigationBarHidden = false
+//        self.tabBarController?.tabBar.isHidden = false
+//        sender.view?.removeFromSuperview()
+//    }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
